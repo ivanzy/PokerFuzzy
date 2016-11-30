521fdc83c5cf4b55d10d39bf8f0c7813535a5f01
ivanzy <idimitryz@gmail.com>
2016-11-30 13:50:52 -0200

    fixed bugs

--- src/pokerfuzzy/Fuzzificador.java
+++ src/pokerfuzzy/Fuzzificador.java
@@ -892,6 +892,10 @@ public class Fuzzificador {
 
         } else /*kicker*/ {
             if (m[0][0] < m[0][1]) {
+                int Temp;
+                Temp = m[0][0];
+                m[0][0] = m[0][1];
+                m[0][1] = Temp;
 
             }
             switch (m[0][0]) {
@@ -949,6 +953,394 @@ public class Fuzzificador {
         return f;
     }
 
+    public static double rateTurn(int[][] m) {
+        double f = 0;
+        if (findSeqFlush(m, 6) != 0) {
+            f = 1;
+        } else if (findQuadra(m, 6) != 0) {
+            f = 0.99;
+        } else if (findFullHouse(m, 6)[0] != 0) {
+            f = 0.98;
+        } else if (findFlush(m, 6)) {
+            f = 0.95;
+        } else if (findSeq(m, 6) != 0) {
+            f = 0.9;
+        } else if (findTrips(m, 6) != 0) {
+            f = 0.8;
+        } else if (findTwoPair(m, 6)[0] != 0) {
+            f = 0.72;
+        } else if (findPair(m, 6) != 0) {
+            switch (findPair(m, 6)) {
+                case 1:
+                    f = 0.72;
+                    break;
+                case 2:
+                    f = 0.5;
+                    break;
+                case 3:
+                    f = 0.52;
+                    break;
+                case 4:
+                    f = 0.53;
+                    break;
+                case 5:
+                    f = 0.55;
+                    break;
+                case 6:
+                    f = 0.57;
+                    break;
+                case 7:
+                    f = 0.6;
+                    break;
+                case 8:
+                    f = 0.62;
+                    break;
+                case 9:
+                    f = 0.64;
+                    break;
+                case 10:
+                    f = 0.65;
+                    break;
+                case 11:
+                    f = 0.69;
+                    break;
+                case 12:
+                    f = 0.7;
+                    break;
+                case 13:
+                    f = 0.72;
+                    break;
+            }
+
+        } else /*kicker*/ {
+            if (m[0][0] < m[0][1]) {
+                int Temp;
+                Temp = m[0][0];
+                m[0][0] = m[0][1];
+                m[0][1] = Temp;
+
+            }
+            switch (m[0][0]) {
+                case 1:
+                    f = 0.42;
+                    break;
+                case 2:
+                    f = 0.22;
+                    break;
+                case 3:
+                    f = 0.3;
+                    break;
+                case 4:
+                    f = 0.32;
+                    break;
+                case 5:
+                    f = 0.32;
+                    break;
+                case 6:
+                    f = 0.34;
+                    break;
+                case 7:
+                    f = 0.35;
+                    break;
+                case 8:
+                    f = 0.37;
+                    break;
+                case 9:
+                    f = 0.38;
+                    break;
+                case 10:
+                    f = 0.39;
+                    break;
+                case 11:
+                    f = 0.4;
+                    break;
+                case 12:
+                    f = 0.4;
+                    break;
+                case 13:
+                    f = 0.4;
+                    break;
+            }
+        }
+
+        if (findFlushDraw(m, 2)) {
+            if (f < 0.75 && f >= 0.55) {
+                f += 0.05;
+            } else if (f < 0.5) {
+                f = 0.5;
+            }
+
+        }
+        return f;
+    }
+
+    public static double rateRiver(int[][] m) {
+        double f = 0;
+        if (findSeqFlush(m, 7) != 0) {
+            f = 1;
+        } else if (findQuadra(m, 7) != 0) {
+            f = 0.99;
+        } else if (findFullHouse(m, 7)[0] != 0) {
+            f = 0.98;
+        } else if (findFlush(m, 7)) {
+            f = 0.95;
+        } else if (findSeq(m, 7) != 0) {
+            f = 0.9;
+        } else if (findTrips(m, 7) != 0) {
+            f = 0.8;
+        } else if (findTwoPair(m, 7)[0] != 0) {
+            f = 0.72;
+        } else if (findPair(m, 7) != 0) {
+            switch (findPair(m, 7)) {
+                case 1:
+                    f = 0.7;
+                    break;
+                case 2:
+                    f = 0.45;
+                    break;
+                case 3:
+                    f = 0.5;
+                    break;
+                case 4:
+                    f = 0.52;
+                    break;
+                case 5:
+                    f = 0.55;
+                    break;
+                case 6:
+                    f = 0.57;
+                    break;
+                case 7:
+                    f = 0.6;
+                    break;
+                case 8:
+                    f = 0.61;
+                    break;
+                case 9:
+                    f = 0.61;
+                    break;
+                case 10:
+                    f = 0.63;
+                    break;
+                case 11:
+                    f = 0.65;
+                    break;
+                case 12:
+                    f = 0.66;
+                    break;
+                case 13:
+                    f = 0.67;
+                    break;
+            }
+
+        } else /*kicker*/ {
+            if (m[0][0] < m[0][1]) {
+                int Temp;
+                Temp = m[0][0];
+                m[0][0] = m[0][1];
+                m[0][1] = Temp;
+
+            }
+            switch (m[0][0]) {
+                case 1:
+                    f = 0.4;
+                    break;
+                case 2:
+                    f = 0.3;
+                    break;
+                case 3:
+                    f = 0.3;
+                    break;
+                case 4:
+                    f = 0.3;
+                    break;
+                case 5:
+                    f = 0.3;
+                    break;
+                case 6:
+                    f = 0.3;
+                    break;
+                case 7:
+                    f = 0.3;
+                    break;
+                case 8:
+                    f = 0.3;
+                    break;
+                case 9:
+                    f = 0.3;
+                    break;
+                case 10:
+                    f = 0.33;
+                    break;
+                case 11:
+                    f = 0.35;
+                    break;
+                case 12:
+                    f = 0.37;
+                    break;
+                case 13:
+                    f = 0.38;
+                    break;
+            }
+        }
+
+        return f;
+    }
+
+    public static double rateWin(int[][] m, int P) {
+        double f = 0;
+        if (findSeqFlush(m, 7) != 0) {
+            f = 1;
+            Propriedades.PMaoFinal[P] = 1;
+        } else if (findQuadra(m, 7) != 0) {
+            f = 0.99;
+            Propriedades.PMaoFinal[P] = 2;
+        } else if (findFullHouse(m, 7)[0] != 0) {
+            f = 0.98;
+            Propriedades.PMaoFinal[P] = 3;
+        } else if (findFlush(m, 7)) {
+            f = 0.95;
+            Propriedades.PMaoFinal[P] = 4;
+        } else if (findSeq(m, 7) != 0) {
+            f = 0.9;
+            Propriedades.PMaoFinal[P] = 5;
+        } else if (findTrips(m, 7) != 0) {
+            f = 0.8;
+            Propriedades.PMaoFinal[P] = 6;
+        } else if (findTwoPair(m, 7)[0] != 0) {
+            f = 0.74;
+            Propriedades.PMaoFinal[P] = 7;
+        } else if (findPair(m, 7) != 0) {
+            switch (findPair(m, 7)) {
+                case 1:
+                    f = 0.74;
+                    Propriedades.PMaoFinal[P] = 8;
+                    break;
+                case 2:
+                    f = 0.5;
+                    Propriedades.PMaoFinal[P] = 20;
+                    break;
+                case 3:
+                    f = 0.53;
+                    Propriedades.PMaoFinal[P] = 19;
+                    break;
+                case 4:
+                    f = 0.58;
+                    Propriedades.PMaoFinal[P] = 18;
+                    break;
+                case 5:
+                    f = 0.6;
+                    Propriedades.PMaoFinal[P] = 17;
+                    break;
+                case 6:
+                    f = 0.62;
+                    Propriedades.PMaoFinal[P] = 16;
+                    break;
+                case 7:
+                    f = 0.64;
+                    Propriedades.PMaoFinal[P] = 15;
+                    break;
+                case 8:
+                    f = 0.65;
+                    Propriedades.PMaoFinal[P] = 14;
+                    break;
+                case 9:
+                    f = 0.66;
+                    Propriedades.PMaoFinal[P] = 13;
+                    break;
+                case 10:
+                    f = 0.67;
+                    Propriedades.PMaoFinal[P] = 12;
+                    break;
+                case 11:
+                    f = 0.7;
+                    Propriedades.PMaoFinal[P] = 11;
+                    break;
+                case 12:
+                    f = 0.72;
+                    Propriedades.PMaoFinal[P] = 10;
+                    break;
+                case 13:
+                    f = 0.73;
+                    Propriedades.PMaoFinal[P] = 9;
+                    break;
+            }
+
+        } else /*kicker*/ {
+            if (m[0][0] < m[0][1]) {
+                int Temp;
+                Temp = m[0][0];
+                m[0][0] = m[0][1];
+                m[0][1] = Temp;
+            }
+            switch (m[0][0]) {
+                case 1:
+                    f = 0.45;
+                    Propriedades.PMaoFinal[P] = 21;
+                    break;
+                case 2:
+                    f = 0.3;
+                    Propriedades.PMaoFinal[P] = 33;
+                    break;
+                case 3:
+                    f = 0.3;
+                    Propriedades.PMaoFinal[P] = 32;
+                    break;
+                case 4:
+                    f = 0.35;
+                    Propriedades.PMaoFinal[P] = 31;
+                    break;
+                case 5:
+                    f = 0.36;
+                    Propriedades.PMaoFinal[P] = 30;
+                    break;
+                case 6:
+                    f = 0.38;
+                    Propriedades.PMaoFinal[P] = 29;
+                    break;
+                case 7:
+                    f = 0.4;
+                    Propriedades.PMaoFinal[P] = 28;
+                    break;
+                case 8:
+                    f = 0.41;
+                    Propriedades.PMaoFinal[P] = 27;
+                    break;
+                case 9:
+                    f = 0.41;
+                    Propriedades.PMaoFinal[P] = 26;
+                    break;
+                case 10:
+                    f = 0.43;
+                    Propriedades.PMaoFinal[P] = 25;
+                    break;
+                case 11:
+                    f = 0.42;
+                    Propriedades.PMaoFinal[P] = 24;
+                    break;
+                case 12:
+                    f = 0.43;
+                    Propriedades.PMaoFinal[P] = 23;
+                    break;
+
+                case 13:
+                    f = 0.44;
+                    Propriedades.PMaoFinal[P] = 22;
+                    break;
+            }
+        }
+
+        if (findFlushDraw(m, 2)) {
+            if (f < 0.75 && f >= 0.55) {
+                f += 0.2;
+            } else if (f < 0.55) {
+                f = 0.62;
+            }
+
+        }
+        return f;
+    }
+
     private static int findPair(int[][] m, int size) {
         int temp, pair = 0;
 
@@ -987,7 +1379,7 @@ public class Fuzzificador {
         return trinca;
     }
 
-    public static int[] findTwoPair(int[][] m, int size) {
+    private static int[] findTwoPair(int[][] m, int size) {
         int temp;
         int[] pair = {0, 0};
 
@@ -1028,14 +1420,14 @@ public class Fuzzificador {
         return isFlush;
     }
 
-    private static boolean findFlushDraw(int[][] m, int size) {
+    public static boolean findFlushDraw(int[][] m, int size) {
         boolean isFlushDraw = false;
         int[] suit = {0, 0, 0, 0};
         for (int i = 0; i < size; i++) {
             suit[m[1][i]]++;
         }
         for (int i = 0; i < 4; i++) {
-            if (suit[i] == 5) {
+            if (suit[i] == 4) {
                 System.out.println("Uma carta para o FLUSH!");
                 isFlushDraw = true;
             }
@@ -1058,7 +1450,7 @@ public class Fuzzificador {
                     maiorC = cont;
                     maiorV = seq[i + 1];
                     //System.out.println(maiorV);
-                    if (maiorV == 13 && seq[0] == 1 && cont==3) {
+                    if (maiorV == 13 && seq[0] == 1 && cont == 3) {
                         maiorV = 1;
                         maiorC++;
                     }
@@ -1130,7 +1522,7 @@ public class Fuzzificador {
     }
 
     private static int findSeqFlush(int[][] m, int size) {
-         int[] seq = new int[size];
+        int[] seq = new int[size];
         int cont = 0, maiorV = 0, maiorC = 0;
         for (int i = 0; i < size; i++) {
             seq[i] = m[0][i];
@@ -1144,7 +1536,7 @@ public class Fuzzificador {
                     maiorC = cont;
                     maiorV = seq[i + 1];
                     //System.out.println(maiorV);
-                    if (maiorV == 13 && seq[0] == 1 && cont==3) {
+                    if (maiorV == 13 && seq[0] == 1 && cont == 3) {
                         maiorV = 1;
                         maiorC++;
                     }
